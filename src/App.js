24f5600df099ee936959845d20e4bedd9e329a23
Hossam Marey <hossam.marey@rytalo.com>
2021-01-18 13:55:25 +0200

    deploing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import ShowPage from "./pages/ShowPage";
+
+import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/shows/:id" component={ShowPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
